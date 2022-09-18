bdf2f234826e8936edf9392bd205c9b6dc553757
Ajaj <ajajrajguru95@gmail.com>
2022-09-18 11:14:40 +0530

    Mobile header responsive completed

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 
-jQuery(document).ready(function($){    });
+jQuery(document).ready(function($){  
+	$('.nav-icon').on('click', function(event){
+       $(this).toggleClass('active');
+       $('body').toggleClass('nav-active overflow-hidden');
+    });
+});
