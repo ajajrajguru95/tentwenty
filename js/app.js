af5c8ff526bdb05b89e361d7155412bf12c94bf8
Ajaj <ajajrajguru95@gmail.com>
2022-09-18 20:37:21 +0530

    Scroll animation with color change handled

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 
+// Query variable to differenciate between mobile and desktop
+let mq = window.matchMedia( "(max-width: 991px)" );
+
 jQuery(document).ready(function($){  
-	// Query variable to differenciate between mobile and desktop
-	let mq = window.matchMedia( "(max-width: 991px)" );
 
 	// Navigation icon click
 	$('.nav-icon').on('click', function(event){
@@ -19,12 +20,62 @@ jQuery(document).ready(function($){
 	          scrollButtons:{
 	              enable:true,
 	              scrollType:"stepped",
-	              scrollAmount: 260
+	              scrollAmount: 300
 	          },
 	          mouseWheel:{ enable: true },
 	          advanced:{ autoExpandHorizontalScroll: true },
 	      });
 	}
-
-
 });
+
+// Intersection Observer to observe the scroll events
+
+const sections = document.querySelectorAll('.tt-main__section');
+
+let config = {
+  rootMargin: '0px',
+  threshold: .5
+};
+
+let observer = new IntersectionObserver((entries) => {
+    // console.log(entries);
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      intersectionHandler(entry);
+    } 
+   console.log(entry);
+  });
+}, config);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+function intersectionHandler(entry) {
+   const current = document.querySelector('.tt-main__section.active');
+  const next = entry.target;
+  // Current Positive color
+  const positive = getComputedStyle(document.body).getPropertyValue('--positive');
+  // Current Negative color
+  const negative = getComputedStyle(document.body).getPropertyValue('--negative');
+
+
+  if (current) {
+    current.classList.remove('active');
+  }
+  if (next) {
+    next.classList.add('active');
+    // document.body.style.setProperty("--color-bg", next.dataset.bgcolor);
+    if((next.classList.contains('section-absolute')) && !(mq.matches)){
+    	document.querySelector('.tt-header').classList.add('negative');
+    	document.body.style.setProperty("--positive", negative);
+    	document.body.style.setProperty("--negative", positive);
+    	document.querySelector('.tt-header').classList.add('negative');
+    }
+    else{
+    	document.body.style.setProperty("--positive", '#fff');
+    	document.body.style.setProperty("--negative", '#000');
+    	document.querySelector('.tt-header').classList.remove('negative');	
+    }
+  }
+}
